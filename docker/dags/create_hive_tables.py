efdeeb912e8ec09b50feb339fed0a79bb38b736a
manuelmontero <manuelms1993@gmail.com>
2024-06-13 21:27:33 +0200

    docker compose airflow

--- docker/dags/create_hive_tables.py
+++ docker/dags/create_hive_tables.py
@@ -14,11 +14,11 @@ default_args = {
 dag = DAG('create_hive_schema_and_tables', default_args=default_args, schedule_interval=None)
 
 # Define the command to execute using docker exec
-command = "docker exec hive-server hive -f /MM_DLK/MyDatalake/hqls/createDB.sql"
+command = "sh /Users/manuelmontero/MM_DLK/MyDatalake/scripts/create_databases_and_tables.sh"
 
 # Define the BashOperator
 hive_task = BashOperator(
-    task_id='hive_task_with_bash_operator',
+    task_id='create_databases_and_tables',
     bash_command=command,
     dag=dag,
 )
---
+++ scripts/create_databases_and_tables.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec hive-server hive -f /MM_DLK/MyDatalake/hqls/createDB.sql
