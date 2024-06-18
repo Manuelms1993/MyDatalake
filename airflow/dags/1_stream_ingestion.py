363be1e7db49ce866c06ee0326094384fbc9e257
manuelmontero <manuelms1993@gmail.com>
2024-06-18 23:51:46 +0200

    changes dags streaming and add hadoop conf

--- airflow/dags/1_stream_ingestion.py
+++ airflow/dags/1_stream_ingestion.py
@@ -10,19 +10,35 @@ default_args = {
     'email_on_retry': False,
     'retry_delay': timedelta(seconds=5),
     'retries': 1,
+    'execution_timeout': timedelta(seconds=30),
 }
 
-dag = DAG('1_stream_hive_ingestion', default_args=default_args,
-          schedule_interval=None, concurrency=10,max_active_runs=1)
+dag = DAG('1_stream_hive_ingestion', default_args=default_args, schedule_interval=None)
 
 # Define the BashOperator
-command3 = "python /Users/manuelmontero/MM_DLK/mydlk-ingestion/main_stream.py "
+command = "sh /Users/manuelmontero/MM_DLK/MyDatalake/scripts/create_topics_kafka.sh "
+create_topic = BashOperator(
+    task_id='create_topic',
+    bash_command=command,
+    dag=dag,
+)
+
+# Define the BashOperator
+command2 = "python /Users/manuelmontero/MM_DLK/mydlk-ingestion/kafka_producer_actors.py &> /dev/null & "
+producer = BashOperator(
+    task_id='execute_producer',
+    bash_command=command2,
+    dag=dag,
+)
+
+
+# Define the BashOperator
+command3 = "python /Users/manuelmontero/MM_DLK/mydlk-ingestion/main_stream.py & "
 consumer = BashOperator(
     task_id='execute_consumer_to_hive',
     bash_command=command3,
     dag=dag,
 )
 
-
 # Define the task dependencies
-consumer
+create_topic >> producer >> consumer
