0a58913825f0503afe8905e91568cec105759670
manuelmontero <manuelms1993@gmail.com>
2024-06-18 20:31:30 +0200

    changes dags streaming

--- airflow/dags/1_stream_ingestion.py
+++ airflow/dags/1_stream_ingestion.py
@@ -15,18 +15,8 @@ default_args = {
 dag = DAG('1_stream_hive_ingestion', default_args=default_args,
           schedule_interval=None, concurrency=10,max_active_runs=1)
 
-# Define the command to execute using docker exec
-
 # Define the BashOperator
-command = "sh /Users/manuelmontero/MM_DLK/MyDatalake/scripts/create_topics_kafka.sh "
-create_topic = BashOperator(
-    task_id='create_topic',
-    bash_command=command,
-    dag=dag,
-)
-
-# Define the BashOperator
-command3 = "python /Users/manuelmontero/MM_DLK/mydlk-ingestion/main_stream.py &> /dev/null & "
+command3 = "python /Users/manuelmontero/MM_DLK/mydlk-ingestion/main_stream.py "
 consumer = BashOperator(
     task_id='execute_consumer_to_hive',
     bash_command=command3,
